723f47b92d2aa17b0ac64035ebc9841c44ffb6fd
mariana <mariamariana.cagnoni@gmail.com>
2023-11-10 21:36:02 -0300

    fix: correcao do @Mapper e implementacao do metodo List  tasksToTaskDTOs

--- src/main/java/com/example/ToDo/list/mapper/TaskMapper.java
+++ src/main/java/com/example/ToDo/list/mapper/TaskMapper.java
@@ -5,17 +5,20 @@ import com.example.ToDo.list.dto.TaskDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 /**
  * @author mariana
  * @Project: To-Do-list
  */
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface TaskMapper {
     TaskMapper INSTANCE = Mappers.getMapper(TaskMapper.class);
 
     TaskDTO taskToTaskDTO(Task task);
 
     Task taskDTOToTask(TaskDTO taskDTO);
+    List<TaskDTO> tasksToTaskDTOs(List<Task> tasks);
 
 }
